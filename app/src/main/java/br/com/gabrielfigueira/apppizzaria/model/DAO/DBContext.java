20e2c3859834c7fee9f9f9d22c24c99c167f16c7
fabricioj <fabricioj_santos@hotmail.com>
2017-06-07 20:47:08 -0300

    Criação do DBContext, procurando unificar a criação da base de dados.

---
+++ app/src/main/java/br/com/gabrielfigueira/apppizzaria/model/DAO/DBContext.java
@@ -0,0 +1,79 @@
+package br.com.gabrielfigueira.apppizzaria.model.DAO;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Fabricio on 07/06/2017.
+ */
+
+public class DBContext extends SQLiteOpenHelper {
+    private final String sql_comanda_create =
+            "create table comanda("+
+                    "id integer primary key AUTOINCREMENT,"+
+                    "mesa text null, " +
+                    "data_hora_abertura text null, " +
+                    "data_sincronizacao text null, " +
+                    "data_hora_finalizacao text null, " +
+                    "desconto real null, " +
+                    "id_centralizado int null, "+
+                    "cliente_id int null"+
+                    ");";
+    private final String sql_comanda_produto_create =
+            "create table comanda_produto("+
+                    "id integer primary key AUTOINCREMENT,"+
+                    "quantidade real null, " +
+                    "data_hora_entrega text null, " +
+                    "comanda_id int null,"+
+                    "produto_id int null"+
+                    ");";
+
+    /*PEGAR COM OS DEMAIS MEMBROS DO GRUPO*/
+    private final String sql_cliente_create = "create table cliente(id integer primary key AUTOINCREMENT, nome text null);";
+    private final String sql_produto_create = "create table produto(id integer primary key AUTOINCREMENT, descricao text null);";
+
+    private SQLiteDatabase db;
+    public DBContext(Context context){
+        super(context,"apppizzaria.db",null, 6);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sql_produto_create);
+        db.execSQL(sql_cliente_create);
+        db.execSQL(sql_comanda_create);
+        db.execSQL(sql_comanda_produto_create);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versaoOld, int versaoNew) {
+        switch (versaoNew){
+            case 1:
+                db.execSQL(sql_produto_create);
+                db.execSQL(sql_cliente_create);
+                db.execSQL(sql_comanda_create);
+                db.execSQL(sql_comanda_produto_create);
+                break;
+            default:
+                if (versaoOld >= 6)
+                    db.execSQL("DROP TABLE comanda;");
+
+                if (versaoOld >= 7)
+                    db.execSQL("DROP TABLE comanda_produto;");
+
+                /*PEGAR AS DEMAIS PARTES COM OS DEMAIS MEMBROS DO GRUPO*/
+                if (versaoOld >= 6)
+                    db.execSQL("DROP TABLE cliente;");
+
+                if (versaoOld >= 7)
+                    db.execSQL("DROP TABLE produto;");
+
+                db.execSQL(sql_cliente_create);
+                db.execSQL(sql_produto_create);
+                db.execSQL(sql_comanda_create);
+                db.execSQL(sql_comanda_produto_create);
+                break;
+        }
+    }
+}
